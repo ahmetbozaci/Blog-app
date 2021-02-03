d10a124fb13cb26fa9085b153559244d3a3e92ef
AymenBida <aymen.bida.00@gmail.com>
2021-02-03 11:15:37 +0100

    Add a new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_151216) do
+ActiveRecord::Schema.define(version: 2021_02_03_092621) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
